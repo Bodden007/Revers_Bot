1405c2f7d76d97f83b485c8ce41435cc86276381
Bodden007 <Denis_Bodrov@mail.ru>
2022-08-19 10:09:26 +0500

    JSON VK

--- src/main/java/reversbot/Main.java
+++ src/main/java/reversbot/Main.java
@@ -1,5 +1,6 @@
 package reversbot;
 
+import com.google.gson.JsonObject;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,14 +8,40 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import reversbot.services.VkBot;
 
+
+import java.io.IOException;
+
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) throws ClientException, ApiException {
+	static String DataText;
+	public String getDataText() {
+		return DataText;
+	}
+
+	public void setDataText(String dataText) {
+		DataText = dataText;
+	}
+
+
+
+
+	public static void main(String[] args) throws ClientException, ApiException, IOException {
 		ApplicationContext context = new AnnotationConfigApplicationContext (InjectionContext.class);
 		VkBot vkBot = context.getBean(VkBot.class);
 
+
+
+
+
 		vkBot.vkb();
+		System.out.println("-----------");
+		System.out.println("-----------");
+
+		System.out.println("&&&&&&&&&&&");
+//		System.out.println(jsonVkBot.toPrettyString(DataText));
+		System.out.println("$$$$$$$$$$$");
+
 	}
 
 }
--- src/main/java/reversbot/services/VkBot.java
+++ src/main/java/reversbot/services/VkBot.java
@@ -1,6 +1,8 @@
 package reversbot.services;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.vk.api.sdk.client.TransportClient;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
@@ -8,28 +10,36 @@ import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.wall.GetFilter;
+import com.vk.api.sdk.objects.wall.Wallpost;
+import com.vk.api.sdk.objects.wall.WallpostFull;
 import com.vk.api.sdk.objects.wall.responses.GetResponse;
+import org.asynchttpclient.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import javax.annotation.PostConstruct;
+
+
+import java.io.IOException;
 
 
 @Service
 
-public class VkBot {
+public class VkBot<jsonString> {
+
+
 
     @Value("${APP_ID}")
     int APP_ID;
 
     @Value("${access_token}")
     String access_token;
+    private String getText;
 
 
+    //    @Scheduled(fixedDelay = 5000)
 
-//    @Scheduled(fixedDelay = 5000)
-    @PostConstruct
-    public void vkb() throws ClientException, ApiException {
+
+    public void vkb() throws ClientException, ApiException, IOException {
 
         TransportClient transportClient = new HttpTransportClient();
         VkApiClient vk = new VkApiClient(transportClient);
@@ -37,17 +47,42 @@ public class VkBot {
 
         UserActor actor = new UserActor(APP_ID, access_token);
 
+
         GetResponse getResponse = vk.wall().get(actor)
-                .ownerId(-206163403)
+                .ownerId(-26493942)
                 .count(1)
                 .offset(5)
                 .filter(GetFilter.valueOf("ALL"))
                 .execute();
 
 
-        System.out.println("........FROM HERE.........");
+
+        String jsonString = getResponse.getItems().get(0).getText();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
+
+//        WallpostFull wallpostFull = gson.fromJson(jsonString, WallpostFull.class);
+
+
+
+
+
+        System.out.println("........ОТСЮДА.........");
         System.out.println(getResponse);
-        System.out.println(".........HERE........");
+        System.out.println(".........СЮДА........");
+        System.out.println(getResponse.getItems().get(0).getText());
+        System.out.println("$-----------$-----------$");
+        System.out.println(")))))))))))))))))");
+        System.out.println(jsonString);
+
+
+
     }
 
+
 }
+
+
+
