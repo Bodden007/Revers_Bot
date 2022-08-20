da982759c1c9989af86bd544ddf57b5533d754fb
Bodden007 <Denis_Bodrov@mail.ru>
2022-08-20 21:30:41 +0500

    Download local text + image

--- src/main/java/reversbot/Main.java
+++ src/main/java/reversbot/Main.java
@@ -1,15 +1,9 @@
 package reversbot;
 
-import com.google.gson.JsonObject;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import reversbot.services.VkBot;
-
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class Main {
@@ -23,24 +17,11 @@ public class Main {
 		DataText = dataText;
 	}
 
-
-
-
-	public static void main(String[] args) throws ClientException, ApiException, IOException {
+	public static void main(String[] args) throws Exception {
 		ApplicationContext context = new AnnotationConfigApplicationContext (InjectionContext.class);
 		VkBot vkBot = context.getBean(VkBot.class);
 
-
-
-
-
 		vkBot.vkb();
-		System.out.println("-----------");
-		System.out.println("-----------");
-
-		System.out.println("&&&&&&&&&&&");
-//		System.out.println(jsonVkBot.toPrettyString(DataText));
-		System.out.println("$$$$$$$$$$$");
 
 	}
 
--- src/main/java/reversbot/services/VkBot.java
+++ src/main/java/reversbot/services/VkBot.java
@@ -1,8 +1,5 @@
 package reversbot.services;
 
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.vk.api.sdk.client.TransportClient;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
@@ -10,23 +7,16 @@ import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.wall.GetFilter;
-import com.vk.api.sdk.objects.wall.Wallpost;
-import com.vk.api.sdk.objects.wall.WallpostFull;
 import com.vk.api.sdk.objects.wall.responses.GetResponse;
-import org.asynchttpclient.Response;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-
-
-import java.io.IOException;
-
+import java.io.*;
+import java.net.URL;
 
 @Service
-
+@Controller
 public class VkBot<jsonString> {
-
-
 
     @Value("${APP_ID}")
     int APP_ID;
@@ -35,39 +25,70 @@ public class VkBot<jsonString> {
     String access_token;
     private String getText;
 
+    @Value("${own_Id}")
+    int own_Id;
 
     //    @Scheduled(fixedDelay = 5000)
 
+    public int vkb() throws ClientException, ApiException, IOException {
 
-    public void vkb() throws ClientException, ApiException, IOException {
+        String urlPhoto = null;
+        String jsonString = null;
+        String fileName = "TestImage.png";
+        GetResponse getResponse = null;
+        FileOutputStream fileOutputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        BufferedWriter writer = null;
 
-        TransportClient transportClient = new HttpTransportClient();
-        VkApiClient vk = new VkApiClient(transportClient);
+        try {
+            TransportClient transportClient = new HttpTransportClient();
+            VkApiClient vk = new VkApiClient(transportClient);
+            UserActor actor = new UserActor(APP_ID, access_token);
+            getResponse = vk.wall().get(actor)
+                    .ownerId(-26493942)
+                    .count(1)
+                    .offset(5)
+                    .filter(GetFilter.valueOf("ALL"))
+                    .execute();
+        }catch (RuntimeException e) {
+            System.out.println("NO HTTP");
+            return 0;
+        }
 
+        try {
+            jsonString = getResponse.getItems().get(0).getText();
+            writer = new BufferedWriter(new FileWriter("VkText.txt"));
+            writer.write(jsonString);
+        } catch (RuntimeException e) {
+            System.out.println("NO TEXT");
+            return 0;
+        } finally {
+            writer.close();
+        }
 
-        UserActor actor = new UserActor(APP_ID, access_token);
+        try {
+            urlPhoto = String.valueOf(getResponse.getItems().get(0).getAttachments().get(0).getPhoto().getSizes().get(8).getUrl());
+        } catch (RuntimeException e) {
+            System.out.println("NO IMAGE");
+            return 0;
+        }
 
-
-        GetResponse getResponse = vk.wall().get(actor)
-                .ownerId(-26493942)
-                .count(1)
-                .offset(5)
-                .filter(GetFilter.valueOf("ALL"))
-                .execute();
-
-
-
-        String jsonString = getResponse.getItems().get(0).getText();
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-
-
-//        WallpostFull wallpostFull = gson.fromJson(jsonString, WallpostFull.class);
-
-
-
-
+        try {
+            bufferedInputStream = new BufferedInputStream(new URL(urlPhoto).openStream());
+            fileOutputStream = new FileOutputStream(fileName);
+            byte data[] = new byte[1024];
+            int count;
+            while ((count = bufferedInputStream.read(data, 0, 1024)) != -1) {
+                fileOutputStream.write(data, 0, count);
+                fileOutputStream.flush();
+            }
+        } catch (RuntimeException e) {
+            System.out.println("NO CONNECT");
+            return 0;
+        } finally {
+            bufferedInputStream.close();
+            fileOutputStream.close();
+        }
 
         System.out.println("........ОТСЮДА.........");
         System.out.println(getResponse);
@@ -75,13 +96,11 @@ public class VkBot<jsonString> {
         System.out.println(getResponse.getItems().get(0).getText());
         System.out.println("$-----------$-----------$");
         System.out.println(")))))))))))))))))");
-        System.out.println(jsonString);
+        System.out.println(urlPhoto);
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%");
 
-
-
+        return 0;
     }
-
-
 }
 
 
