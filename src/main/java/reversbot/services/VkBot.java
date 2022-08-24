fb99770df29abb203713c9ad0fcc94996ff13de5
Bodden007 <Denis_Bodrov@mail.ru>
2022-08-24 21:02:14 +0500

    Working on logic.

--- src/main/java/reversbot/services/VkBot.java
+++ src/main/java/reversbot/services/VkBot.java
@@ -8,6 +8,7 @@ import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.wall.GetFilter;
 import com.vk.api.sdk.objects.wall.responses.GetResponse;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.io.*;
@@ -26,24 +27,40 @@ public class VkBot<jsonString> {
     @Value("${own_Id}")
     int own_Id;
 
+    static int [] postIdOld = new int[5];
+    static int [] postId = new int[5];
+
+    static String [] urlPhoto = new String[10];
+
+    static int phelp = 0;
+
+    static String typeAtt = "null";
+    static String typeAttPhoto = "photo";
+    static String typeAttVideo = "video";
+
+
+
     //    @Scheduled(fixedDelay = 5000)
 
     public int vkb() throws ClientException, ApiException, IOException {
 
+        String text = null;
         Integer numberGet = 0;
-        String typeAttPost = "post";
-        String typeAttPhoto = "photo";
-        String typeAttVideo = "video";
-        String typeAtt = null;
-        String urlPhoto = null;
+        String post_sours = null;
         String jsonString = null;
         String fileName = "TestImage";
         GetResponse getResponse = null;
         FileOutputStream fileOutputStream = null;
         BufferedInputStream bufferedInputStream = null;
+        BufferedReader reader = null;
         BufferedWriter writer = null;
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // Чистим кэш удаляем из деректории text & image
 
+        FileUtils.cleanDirectory(new File("src/cache/text"));
+        FileUtils.cleanDirectory(new File("src/cache/image"));
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         try {
             TransportClient transportClient = new HttpTransportClient();
@@ -60,100 +77,156 @@ public class VkBot<jsonString> {
             return 0;
         }
 
+        // Забераем данные ID с cache для сравнения
 
+        try {
+            reader = new BufferedReader(new FileReader("src/cache/oldId/OldId.txt"));
+            for (int i = 0; i <= 4; i ++) {
+                postIdOld [i] = Integer.parseInt(reader.readLine());
+            }
+        } catch (RuntimeException e) {
+            System.out.println("reader faile");
+        } finally {
+            reader.close();
+        }
         //Начало основного цикла.
 
-        for ( numberGet = 0; numberGet < 5; numberGet ++) {
+            for ( numberGet = 4; numberGet >= 0; numberGet --) {
 
-        // определения типа сообщения text, video, post
+                phelp = 0;
 
-            try {
-                typeAtt = String.valueOf(getResponse.getItems().get(numberGet).getAttachments().get(0).getType());
-            } catch (RuntimeException e) {
-                typeAtt = String.valueOf(getResponse.getItems().get(1).getPostType());
-                System.out.println(numberGet  + " " + typeAtt);
-            }
+                // чтение последнего ID из файла
 
-        // чтение в переменную jsonString, text из JSON
+                postId[numberGet] = getResponse.getItems().get(numberGet).getId();
 
-            try {
-                jsonString = getResponse.getItems().get(numberGet).getText();
-                writer = new BufferedWriter(new FileWriter("src/text/VkText" + numberGet + ".txt"));
-                if (numberGet.equals(0)) {
-                    writer.write(jsonString);
-                }else {
-                        writer.append(jsonString);
+                //сравнение ID на повторяемость запросов
+
+
+                if ((postId[numberGet] > postIdOld[4]) & (postId[numberGet] > postIdOld[3])
+                        & (postId[numberGet] > postIdOld[2]) & (postId[numberGet] > postIdOld[1])
+                        & (postId[numberGet] > postIdOld[0])) {
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                    // определение наличие text. Если text нет, сообщение игнорируем
+
+                    // Test
+                    System.out.println(numberGet + "  ID " + postId[numberGet]);
+                    //Test
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                         Определяем тип вложений PHOTO & VIDEO
+                    try {
+                        typeAtt = String.valueOf(getResponse.getItems().get(numberGet)
+                                .getAttachments().get(0).getType());
+                        System.out.println("It's  " + typeAtt);
+                    }catch (RuntimeException e) {
+                        System.out.println("The type is not defined");
                     }
 
-            } catch (RuntimeException e) {
-                System.out.println(numberGet + " NO TEXT");
-                return 0;
-            } finally {
-                writer.close();
-            }
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-            if (typeAtt.equals(typeAttPost)) {
-
-                System.out.println(numberGet + " " + typeAttPost);
-
-        // Если тип сообщения text, вытаскиваем из него картинки
-
-            } else if (typeAtt.equals(typeAttPhoto)) {
-                System.out.println(numberGet + " " + typeAttPhoto);
-                try {
-                    urlPhoto = String.valueOf(getResponse.getItems().get(numberGet).getAttachments().get(0).getPhoto().getSizes().get(8).getUrl());
-                    System.out.println(urlPhoto);
-                } catch (RuntimeException e) {
-                    urlPhoto = String.valueOf(getResponse.getItems().get(numberGet).getAttachments().get(8).getPhoto().getSizes().get(8).getUrl());
-                    System.out.println("NO IMAGE");
-
-                }
-
-            // сохраняем картинки локально
-
-                try {
-                    bufferedInputStream = new BufferedInputStream(new URL(urlPhoto).openStream());
-                    fileOutputStream = new FileOutputStream("src/image/"+ fileName + numberGet + ".png");
-                    byte data[] = new byte[1024];
-                    int count;
-                    while ((count = bufferedInputStream.read(data, 0, 1024)) != -1) {
-                        fileOutputStream.write(data, 0, count);
-                        fileOutputStream.flush();
+                    try {
+                        text = getResponse.getItems().get(numberGet).getText();
+                    } catch (RuntimeException e) {
+                        text = null;
                     }
-                } catch (RuntimeException e) {
-                    System.out.println("NO CONNECT");
-                    return 0;
-                } finally {
-                    bufferedInputStream.close();
-                    fileOutputStream.close();
-                }
-            // Если тип сообщения video, удаляем фото
 
-            } else if (typeAtt.equals(typeAttVideo)) {
-                File file = new File("src/image/"+ fileName + numberGet + ".png");
-                file.delete();
-                System.out.println(numberGet + " It is video");
 
-            } else {
-                System.out.println("NO ATTACHMENT");
-            }
+                        if (!(text.equals(""))) {
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+                        System.out.println("ID " + postId[numberGet] + " Text Ok! -- " + text);
+
+                        // чтение в переменную jsonString, text из JSON
+                        //считываем сообщение в файл
+
+                        try {
+                            jsonString = getResponse.getItems().get(numberGet).getText();
+                            writer = new BufferedWriter(new FileWriter("src/cache/text/VkText" + numberGet + ".txt"));
+                            if (numberGet.equals(0)) {
+                                writer.write(jsonString);
+                            } else {
+                                writer.append(jsonString);
+                            }
+                        } catch (RuntimeException e) {
+                            System.out.println(numberGet + " NO TEXT");
+                            return 0;
+                        } finally {
+                            writer.close();
+                        }
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+                        // Если есть photo копируем URL photo
+
+                        if (typeAtt.equals(typeAttPhoto)) {
+                            for (int i = 0; i < 20; i++) {
+                                try {
+                                    urlPhoto[i] = String.valueOf(getResponse.getItems().get(numberGet)
+                                            .getAttachments().get(i).getPhoto()
+                                            .getSizes().get(8).getUrl());
+                                    phelp = phelp + 1;
+                                    System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                    System.out.println(i);
+                                    System.out.println(urlPhoto[i]);
+                                    System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                                } catch (RuntimeException e) {
+                                    System.out.println("Quantity IMAGE -  " + phelp);
+                                    break;
+                                }
+                            }
+
+                          //   Если есть video копируем URL video
+                        }else if (typeAtt.equals(typeAttVideo)) {
+                            
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                         // это заглушка video
+                            System.out.println("It's video");
+                        }
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                                // если картинка есть сохраняем локально
+
+                         if (typeAtt.equals(typeAttPhoto)) {
+
+                             // TEST
+                             System.out.println(postId[numberGet] + "  Image Yes");
+                             // TEST
+
+                            for ( int i = 0; i <= (phelp - 1); i++) {
+
+                                try {
+                                    bufferedInputStream = new BufferedInputStream(new URL(urlPhoto[i]).openStream());
+                                    fileOutputStream = new FileOutputStream("src/cache/image/" + fileName
+                                            + numberGet + "_" + i + ".png");
+                                    byte data[] = new byte[1024];
+                                    int count;
+                                    while ((count = bufferedInputStream.read(data, 0, 1024)) != -1) {
+                                        fileOutputStream.write(data, 0, count);
+                                        fileOutputStream.flush();
+                                    }
+                                } catch (RuntimeException e) {
+                                    System.out.println("Saving error");
+                                    break;
+                                } finally {
+                                    bufferedInputStream.close();
+                                    fileOutputStream.close();
+                                }
+                            }
+                        }
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                        }else {
+                        System.out.println("Post  " + postId[numberGet] + "  Out");
+                        }
+
+                }else{
+                    postId[numberGet] = 0;
+                    System.out.println(numberGet + "  Old message");
+             }
         }
-
-        System.out.println("........ОТСЮДА.........");
-        System.out.println(getResponse);
-        System.out.println(".........СЮДА........");
-        System.out.println(getResponse.getItems().get(0).getText());
-        System.out.println("$-----------$-----------$");
-        System.out.println(")))))))))))))))))");
-        System.out.println(urlPhoto);
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%");
-        System.out.println(typeAtt);
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%");
-        System.out.println(jsonString);
-        System.out.println("))))))))))))))))))))))");
-
-        return 0;
-    }
+    return 0;
+   }
 }
 
 
